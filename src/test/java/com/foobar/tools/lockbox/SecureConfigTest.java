e572b813ae603c912d4a30a831ff5864fcefd5b1
Michael Shanzer <shanzer@foobar.com>
2024-02-22 14:35:26 -0500

    start of test code

--- src/test/java/com/foobar/tools/lockbox/SecureConfigTest.java
+++ src/test/java/com/foobar/tools/lockbox/SecureConfigTest.java
@@ -5,17 +5,30 @@ import org.junit.Test;
 public class SecureConfigTest {
     @Test
     public void testInitialize() {
-
+        // null file name
+        // non existant file name
+        // bad / garbage file
+        // no protected parmeters
+        // missing required parameters
+        // null application name
+        // different application key
+        // good locked config file (right system)
+        // bad locked config file (copied from another system)
     }
 
     @Test
     public void testGetProperties() {
-
+        // Get protected parameter
+        // get clear parameter
+        // get non-existant parameter
     }
 
     @Test
     public void testIsProtected() {
-
+        // protected file
+        // unprotected file
+        // newly protected file
+        // just resteted file
     }
 
     @Test
